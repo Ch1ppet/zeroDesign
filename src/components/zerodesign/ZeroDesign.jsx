e559627ecd42481843cd553a323cbdca0a1ac57b
Ch1ppet <zenaillarionov8245@gmail.com>
2025-04-09 00:05:43 +0400

    zero design section is done

--- src/components/zerodesign/ZeroDesign.jsx
+++ src/components/zerodesign/ZeroDesign.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import ZeroDesignInfo from "./ZeroDesignInfo.jsx";
+import './zerodesignStyles/zeroDesign.scss';
+import './zerodesignStyles/zeroDesignInfo.scss';
+import img from '../../../public/assets/img/zero-design-section/zeroDesignImg.jpg';
 
 const ZeroDesign = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="zero-design">
+            <div className="container zero-design__container">
+                <div className="zero-design__left">
+                    <img src={img} alt="" />
+                </div>
+                <div className="zero-design__right">
+                    <h2 className="zero-design__title">
+                        Zero Design
+                    </h2>
+                    <p className="zero-design__text">
+                        Includes a total of 10 static pages, divided like this: 4 Landing pages, 2 About pages, 2 Contact page, 1 Team page, 1 FAQ page. In addition to these, there are Webflow CMS driven pages: Blog (in 2 variations), Shop (in 2 variations), Works and Careers. All with their respective single pages!
+                    </p>
+                    <ZeroDesignInfo />
+                </div>
+            </div>
+        </section>
     );
 };
 
---
+++ src/components/zerodesign/ZeroDesignInfo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ZeroDesignInfo = () => {
+    return (
+        <div className="zero-design__info">
+            <div className="zero-design__info-top">
+                <span className="zero-design__info-item">4 LANDING</span>
+                <span className="zero-design__info-item">2 ABOUT</span>
+                <span className="zero-design__info-item">3 CONTACT</span>
+                <span className="zero-design__info-item">3 BLOG</span>
+                <span className="zero-design__info-item">2 SHOP</span>
+            </div>
+            <div className="zero-design__info-bottom">
+                <span className="zero-design__info-item">WORKS</span>
+                <span className="zero-design__info-item">TEAM</span>
+                <span className="zero-design__info-item">CAREERS</span>
+                <span className="zero-design__info-item">FORMS</span>
+                <span className="zero-design__info-item">UTILITY</span>
+            </div>
+        </div>
+    );
+};
+
+export default ZeroDesignInfo;
